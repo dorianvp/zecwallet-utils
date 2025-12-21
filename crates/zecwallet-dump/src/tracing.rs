9f203904cb706fb535a405fc1131cf2158cfddbe
dorianvp <dorianvp@zingolabs.org>
2025-12-21 00:49:08 -0300

    chore: reduce logging noise

--- crates/zecwallet-dump/src/tracing.rs
+++ crates/zecwallet-dump/src/tracing.rs
@@ -13,7 +13,7 @@ pub(crate) fn init_tracing(debug: u8) {
 
     tracing_subscriber::fmt()
         .with_env_filter(filter)
-        .with_span_events(FmtSpan::FULL)
+        .with_span_events(FmtSpan::ACTIVE)
         .with_target(false)
         .compact()
         .init();
